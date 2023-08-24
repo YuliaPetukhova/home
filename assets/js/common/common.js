4554299020ebc061f2b3841cfe9fc873798f2b70
YuliaPetukhova <yuljaschik99@gmail.com>
2023-08-24 15:20:57 +0500

    new class

--- assets/js/common/common.js
+++ assets/js/common/common.js
@@ -1,21 +1,72 @@
-document.addEventListener("DOMContentLoaded", () => {
-    let bottomMenu = document.querySelector("#bottom-menu");
-    let prevPageYOffset = window.pageYOffset;
-    let timeout;
+'use strict';
 
-    window.onscroll = function () {
-        if (timeout) {
-            window.clearTimeout(timeout);
-            timeout = null;
+class BottomMenu
+{
+    SHOW_TIMER = 2000;
+
+    timeout;
+    bottomMenu;
+    prevPageYOffset;
+
+    constructor() {
+        this.bottomMenu = document.querySelector("#bottom-menu");
+        this.prevPageYOffset = window.pageYOffset;
+    }
+
+    clearTimeout() {
+        if (this.timeout) {
+            window.clearTimeout(this.timeout);
         }
-        if (window.pageYOffset > prevPageYOffset) {
-            bottomMenu.style.display = "none";
-            timeout = window.setTimeout(function(){
-                  bottomMenu.style.display = "block";
-              }, 2000);
-        } else {
-            bottomMenu.style.display = "block";
+    }
+    
+    hideMenu() {
+        this.bottomMenu.style.display = "none";
+    }
+
+    showMenu() {
+        this.bottomMenu.style.display = "block";
+    }
+
+    handleScroll() {
+        this.visibilityMenuOnScroll();
+    }
+    
+    handleScrollUp() {
+        if (window.pageYOffset < this.prevPageYOffset) {
+            this.showMenu();
         }
-        prevPageYOffset = window.pageYOffset;
-    } 
-})
+    }
+
+    handleScrollDown() {
+        let self = this;
+
+        if (window.pageYOffset > this.prevPageYOffset) {
+            this.hideMenu();
+            this.timeout = window.setTimeout(function(){
+                self.showMenu();
+            }, self.SHOW_TIMER);
+        }
+    }
+
+    resetPrevPageYOffset() {
+        this.prevPageYOffset = window.pageYOffset;
+    }
+
+    visibilityMenuOnScroll() {
+        let self = this;
+
+        window.onscroll = function () {
+            self.clearTimeout();
+            self.handleScrollUp();
+            self.handleScrollDown();
+            self.resetPrevPageYOffset();
+        }   
+    }
+
+    init() {
+        this.handleScroll();
+    }
+}
+
+let bottomMenuObject = new BottomMenu();
+bottomMenuObject.init();
--- assets/js/task-list/class/home-task-list.js
+++ assets/js/task-list/class/home-task-list.js
@@ -1,6 +1,7 @@
 'use strict';
 
-export class HomeTaskList{
+export class HomeTaskList
+{
     ExpandTasksOnClick() {
         const taskCells = document.querySelectorAll('td.task');
         
